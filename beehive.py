93b94ee555f20d073aa7407c1c8ccb4de4498880
edwa0003 <edwa0003@student.monash.edu>
2023-05-21 12:24:57 +0800

    menambahkan method lt, eq, dan le di Beehive
    Panen hive. Lalu masukkan hive yang baru saja dipanen dengan volume yang telah dikurangkan ke dalam hive_tree.

--- beehive.py
+++ beehive.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from heap import MaxHeap
-from bst import BinarySearchTree
+from threedeebeetree import ThreeDeeBeeTree
 
 @dataclass
 class Beehive:
@@ -14,29 +14,42 @@ class Beehive:
     nutrient_factor: int
     volume: int = 0
 
+    def __lt__(self, other):
+        return min(self.capacity, self.volume) * self.nutrient_factor < min(other.capacity, other.volume) * other.nutrient_factor
+
+    def __eq__(self, other):
+        return min(self.capacity, self.volume) * self.nutrient_factor == min(other.capacity,other.volume) * other.nutrient_factor
+
+    def __le__(self, other):
+        return self.__lt__(other) or self.__eq__(other)
+
 class BeehiveSelector:
 
     def __init__(self, max_beehives: int):
         self.emerald_per_day_heap=MaxHeap(max_beehives) #a heap of how much emeralds per day can be gained
-        self.hive_tree=BinarySearchTree()
+        self.hive_tree=ThreeDeeBeeTree()
 
     def set_all_beehives(self, hive_list: list[Beehive]):
-        self.hive_tree = BinarySearchTree()
+        self.hive_tree = ThreeDeeBeeTree()
         for hive in hive_list:
-            emerald_per_day = min(hive.capacity, hive.volume) * hive.nutrient_factor
+            self.emerald_per_day_heap.add(hive)
             self.hive_tree[(hive.x,hive.y,hive.z)]=hive
     
     def add_beehive(self, hive: Beehive):
-        emerald_per_day = min(hive.capacity, hive.volume) * hive.nutrient_factor #the amount of emeralds you can gain is volume_harvested*nutrient_factor
-        self.emerald_per_day_heap.add(emerald_per_day)
-        self.hive_tree[emerald_per_day] = hive
+        self.emerald_per_day_heap.add(hive)
+        self.hive_tree[(hive.x, hive.y, hive.z)] = hive
     
     def harvest_best_beehive(self)-> float:
-        emeralds_gained=self.emerald_per_day_heap.get_max()
-        harvested_hive=self.hive_tree[emeralds_gained]
-        volume_harvested=emeralds_gained/harvested_hive.volume
-        hive_after_harvest=Beehive(harvested_hive.capacity,harvested_hive.nutrient_factor,harvested_hive.volume-volume_harvested)
-        emerald_per_day = min(hive.capacity, hive.volume) * hive.nutrient_factor
-
+        harvested_hive=self.emerald_per_day_heap.get_max()
+        return harvested_hive
 
 if __name__ == '__main__':
+    b1=Beehive(15, 12, 13, capacity=40, nutrient_factor=5, volume=15) #emerald per day 75
+    b2=Beehive(25, 22, 23, capacity=15, nutrient_factor=8, volume=40) #emerald per day 120
+    b3=Beehive(35, 32, 33, capacity=40, nutrient_factor=3, volume=40) #emerald per day 120
+    b4=Beehive(45, 42, 43, capacity=1, nutrient_factor=85, volume=10) #emerald per day 85
+    b5=Beehive(55, 52, 53, capacity=400, nutrient_factor=5000, volume=0)  #emerald per day 0
+    beehive_selector=BeehiveSelector(5)
+    beehive_selector.set_all_beehives([b1,b2,b3,b4,b5])
+    print(beehive_selector.harvest_best_beehive())
+    print(b2<=b3)
